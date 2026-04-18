c091b4c4197186f8b37832e31551547abd3bd1ae
Patrick Gehrsitz <github@mryel.de>
2026-04-18 20:22:01 +0200

    refactor: remove another else block
    Signed-off-by: Patrick Gehrsitz <github@mryel.de>

--- spyglass/__init__.py
+++ spyglass/__init__.py
@@ -8,10 +8,9 @@ from picamera2.encoders import _hw_encoder_available
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+WEBRTC_ENABLED = False
 if importlib.util.find_spec("aiortc"):
     WEBRTC_ENABLED = _hw_encoder_available
-else:
-    WEBRTC_ENABLED = False
 
 
 def set_webrtc_enabled(enabled):
